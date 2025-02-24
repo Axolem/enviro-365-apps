b27f05dd8a967065183f7dfa3ecc082920cadf55
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-24 22:58:38 +0200

    feat: complete nav bar

--- webapp/src/app/components/header/header.component.ts
+++ webapp/src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  isMenuOpen = false
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen
+  }
 }
